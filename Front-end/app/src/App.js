91e2e219bd42028c8e8634065944e19637e038cc
Ahmad Noor <99959745+Ahmad-ft-215@users.noreply.github.com>
2022-06-15 11:41:20 +0530

    footer

--- Front-end/app/src/App.js
+++ Front-end/app/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import Footer from './components/Footer'
 // import { Series } from './components/Series/currentcricket';
 
 function App() {
   return (
     <div className="App">
-        
+      <Footer />  
     </div>
   );
 }
