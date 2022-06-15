9edbaba5403faaf5a9d6ed846031987ac0cb34d2
rushig777 <gaikwadrushi777@gmail.com>
2022-06-15 10:54:47 +0530

    add

--- teams/my-team/src/pages/SingleTeam.jsx
+++ teams/my-team/src/pages/SingleTeam.jsx
@@ -2,9 +2,13 @@
 import React from 'react'
 import styles from "../css/team.module.css"
 import teams from "../data/teams.json"
-import { Params, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import teamMatchesData from "../data/schedules.json"
+import { Match } from '../components/match'
 export const SingleTeam = () => {
     let {team} =useParams()
+    let schedulearr = teamMatchesData.teamMatchesData;
+    console.log(schedulearr[0].matchDetailsMap.match)
      let teamarr=teams.teams;
   return (
 
@@ -33,7 +37,22 @@ export const SingleTeam = () => {
     </div>
     </div>
     <div className={styles.mainBox}>
-        <div className={styles.matches}></div>
+        <div className={styles.matches}>
+            <div className={styles.toggle}>
+                <div>Fixtures</div>
+                <div>Results</div>
+            </div>
+            <div>
+                {
+    schedulearr.map((el,i)=>{
+        return <div key={i}>
+          <Match{...el}  />
+        </div>
+    })
+
+                }
+            </div>
+        </div>
         <div className={styles.news}></div>
         <div className={styles.ranking}></div>
     </div>
