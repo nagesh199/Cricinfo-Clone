adcc9bf031221c0eaa5724ed2ece1e8d1c449165
Ahmad Noor <99959745+Ahmad-ft-215@users.noreply.github.com>
2022-06-16 11:49:49 +0530

    footer

--- Front-end/app/src/components/ScoreCard.jsx
+++ Front-end/app/src/components/ScoreCard.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import styles from "./ScoreCard.module.css"
 
-const ScoreCard = ({ team1Img, live, category, team2Score}) => {
-    //console.log(props)
+const ScoreCard = (props) => {
+    console.log(props)
     return (
-    <div className="itemContainer">
-      <div className={styles.itemImg}>
-        <img src={team1Img} alt={live} />
-      </div>
-      <div className={styles.itemInfo}>
-        <p className={styles.brand}>{live}</p>
-        <p className={styles.title}>{category}</p>
-        <p className={styles.price}>₹ {team2Score}</p>
-      </div>
-    </div>
+    //   { team1Img, live, category, team2Score}
+    // <div className="itemContainer">
+    //   <div className={styles.itemImg}>
+    //     <img src={team1Img} alt={live} />
+    //   </div>
+    //   <div className={styles.itemInfo}>
+    //     <p className={styles.brand}>{live}</p>
+    //     <p className={styles.title}>{category}</p>
+    //     <p className={styles.price}> {team2Score}</p>
+    //   </div>
+    // </div>
+    <div></div>
     );
 }
 
