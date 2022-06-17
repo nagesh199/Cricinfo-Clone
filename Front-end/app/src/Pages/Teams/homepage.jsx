8884ea7aa49d8a370b9673e942a6e07a72dd4ea9
rushig777 <gaikwadrushi777@gmail.com>
2022-06-17 18:51:37 +0530

    .deleted

--- Front-end/app/src/Pages/Teams/homepage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { HomeBox } from '../components/HomeBox';
-import HomeNews from "../../../../../New folder/src/data/homepage.json"
-export const Homepage = () => {
-    
-  return (
-  <div></div>
-  )
-}
