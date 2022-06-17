1bb978a76b7b256ee4eda2bbecb845168c7146d2
nagesh199 <99637018+nagesh199@users.noreply.github.com>
2022-06-17 22:06:38 +0530

    change route

--- Front-end/app/src/Routes/Mainroutes.jsx
+++ Front-end/app/src/Routes/Mainroutes.jsx
@@ -4,12 +4,8 @@ import { Route, Routes } from 'react-router-dom'
 import Footer from '../components/header-footer/Footer'
 import Navbar from '../components/header-footer/Navbar'
 import { Home } from '../components/Home/Home'
-import { Current } from '../components/Series/current'
 import { Series } from '../components/Series/currentcricket'
-import { Future } from '../components/Series/future'
 import { India } from '../components/Series/india'
-import { Recent } from '../components/Series/recent'
-import { Result } from '../components/Series/result'
 import { SingleTeam } from '../Pages/Teams/SingleTeam'
 import { Teams } from '../Pages/Teams/teams'
 export const Mainroutes = () => {
@@ -25,11 +21,7 @@ export const Mainroutes = () => {
        <Navbar/>
    <Routes>
     <Route path="/" element={<Home theme={theme} />}/>
-    <Route path="/series/current" element={<Current/>}/>
     <Route path="/series" element={<Series/>}/>
-    <Route path="/series/future" element={<Future/>}/>
-    <Route path="/series/recent" element={<Recent/>}/>
-    <Route path="/series/result" element={<Result/>}/>
     <Route path='/teams' element={<Teams/>}></Route>
     <Route path ="/series/india" element={<India/>}></Route>
     <Route path='/teams/:team' element={<SingleTeam/>}></Route>
