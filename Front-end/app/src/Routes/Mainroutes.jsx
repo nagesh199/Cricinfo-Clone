457b5a018e976c4e3ae4d281e55691086d330292
abhinavroy121 <abhinavroy121.ar@gmail.com>
2022-06-17 23:05:57 +0530

    updated

--- Front-end/app/src/Routes/Mainroutes.jsx
+++ Front-end/app/src/Routes/Mainroutes.jsx
@@ -5,12 +5,8 @@ import Footer from '../components/header-footer/Footer'
 import Header from '../components/header-footer/Header'
 import Navbar from '../components/header-footer/Navbar'
 import { Home } from '../components/Home/Home'
-<<<<<<< HEAD
 import Individualdata from '../components/News/Individualdata'
 import Main from '../components/News/Main'
-import { Current } from '../components/Series/current'
-=======
->>>>>>> a7aa882857816ca763d8c33c46b836580e75b0f8
 import { Series } from '../components/Series/currentcricket'
 import { India } from '../components/Series/india'
 import { SingleTeam } from '../Pages/Teams/SingleTeam'
@@ -30,14 +26,9 @@ export const Mainroutes = () => {
    <Routes>
     <Route path="/" element={<Home theme={theme} />}/>
     <Route path="/series" element={<Series/>}/>
-<<<<<<< HEAD
-  
+    <Route path ="/news" element={<Main/>}/>
+    <Route path="/news/:id" element={<Individualdata/>}/>
       {/* <Route path="/livescores" element={<LiveScores/>}/> */}
-=======
-    <Route path='/teams' element={<Teams/>}></Route>
-    <Route path ="/series/india" element={<India/>}></Route>
-    <Route path='/teams/:team' element={<SingleTeam/>}></Route>
->>>>>>> a7aa882857816ca763d8c33c46b836580e75b0f8
    </Routes>
    <Footer/>
     </div>
