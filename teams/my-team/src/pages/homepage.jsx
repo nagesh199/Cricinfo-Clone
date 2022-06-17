ecf5fd20a478634d7c8dc2122ed7f82b856a3549
rushig777 <gaikwadrushi777@gmail.com>
2022-06-17 17:55:04 +0530

    ..

--- teams/my-team/src/pages/homepage.jsx
+++ teams/my-team/src/pages/homepage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import { HomeBox } from '../components/HomeBox';
+import HomeNews from "../data/homepage.json"
 export const Homepage = () => {
+    const  newsarr =HomeNews.HomeNews;
   return (
-    <div>Homepage</div>
+    <div>
+        {newsarr.map((el,i)=>{
+            return <div key={i}>
+            <HomeBox {...el} />
+            </div>
+        })}
+    </div>
   )
 }
