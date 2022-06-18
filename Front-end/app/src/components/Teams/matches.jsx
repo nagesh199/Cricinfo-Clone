2b683db5854bf09dabb29759e5ddb06c9e64d7ac
rushig777 <gaikwadrushi777@gmail.com>
2022-06-18 12:29:25 +0530

    ..hover added

--- Front-end/app/src/components/Teams/matches.jsx
+++ Front-end/app/src/components/Teams/matches.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { RiArrowRightSLine } from "react-icons/ri";
-
+import styles from '../../CSS/Teams/homepage.module.css'
 export const Matches = ({ list }) => {
   return (
     <>
       {list.map((el,i) => {
         return (
-           <div key={i}>
+           <div key={i} className={styles.sublist}>
             <RiArrowRightSLine style={{ color: " rgb(3,169,244)" }} />
-          <span>{el.vs}</span>
+           <span>{el.vs}</span>
           </div>
         );
       })}
