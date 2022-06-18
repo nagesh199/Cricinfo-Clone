7fc473e1542a027ba9f88aaa09249a74f4acebae
abhinavroy121 <abhinavroy121.ar@gmail.com>
2022-06-18 13:07:14 +0530

    added

--- Front-end/app/src/components/Teams/HomeBox.jsx
+++ Front-end/app/src/components/Teams/HomeBox.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from "../../CSS/Teams/homepage.module.css"
-export const HomeBox = ({storyList}) => {
+export const HomeBox = ({storyList,theme}) => {
    
   return (
     <div>
@@ -8,13 +8,13 @@ export const HomeBox = ({storyList}) => {
                 
             return <div key={i}>
             <img src={story.imageId} alt="" />   
-            <h3>{story.hline}</h3>
-             <p>{story.intro}</p>
+            <h3 id={theme ?styles.day : styles.night}>{story.hline}</h3>
+             <p id={theme ?styles.day : styles.night}>{story.intro}</p>
             <div className={styles.slider}>
               {story.sublist.map(({story},i)=>{
                 return <div>
                    <img src={story.imageId} alt="" />   
-                   <p>{story.seoHeadline}</p>
+                   <p id={theme ?styles.day : styles.night}>{story.seoHeadline}</p>
                 </div>
               })}
             </div>
@@ -23,8 +23,8 @@ export const HomeBox = ({storyList}) => {
               <img src={story.source.img} alt="" />
               </div>
               <div>
-                <h3>{story.source.caption}</h3>
-                <p>{story.source.subCaption}</p>
+                <h3 id={theme ?styles.day : styles.night}>{story.source.caption}</h3>
+                <p id={theme ?styles.day : styles.night}>{story.source.subCaption}</p>
               </div>
             </div>
             </div>
