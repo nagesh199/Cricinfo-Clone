6bbcc384455e1305b7f00901378f56daf832d5ea
rushig777 <gaikwadrushi777@gmail.com>
2022-06-18 16:56:47 +0530

    ..updated backend

--- Front-end/app/src/Pages/Teams/teams.jsx
+++ Front-end/app/src/Pages/Teams/teams.jsx
@@ -2,23 +2,36 @@ import { Team } from "../../components/Teams/team";
 import list from "../../DATABASE/Teams/db.json";
 import styles from "../../CSS/Teams/teams.module.css";
 import { useNavigate } from "react-router-dom";
+import axios from "axios"
+import { useEffect } from "react";
+import { useState } from "react";
 export const Teams = ({theme}) => {
-  let teamsarr = list.list;
-  let womenarr = list.womenlist;
-  let iplarr = list.ipllist;
+
+  useEffect(() => { 
+    getData();
+  },[])
+const [iplData,setipl]=useState([])
+const [teamData,setTeam]=useState([])
+const [womenData,setwData]=useState([])
 
   const navigate=useNavigate();
   const handleClick=(el)=>{
-
   navigate(`/teams/${el.teamName}`)
-
   }
+const getData= async()=>{
+  const res= await axios.get("http://localhost:3030/teams")
+  console.log(res.data.ipllist)
+  setipl(res.data.ipllist)
+  setTeam(res.data.list)
+  setwData(res.data.womenlist)
+}
+
   return (
     <div id={theme ?styles.day : styles.night}>
       <div  className={styles.container}>
         <span>POPULAR INTERNATIONAL TEAMS</span>
         <div className={styles.mainDiv}>
-          {teamsarr.map((el, i) => {
+          {teamData.map((el, i) => {
             return (
               <div id={theme ?styles.day : styles.night} key={i} onClick={()=>handleClick(el)}>
                 <Team {...el} />
@@ -30,7 +43,7 @@ export const Teams = ({theme}) => {
       <div className={styles.container}>
         <span>POPULAR WOMEN'S TEAMS</span>
         <div className={styles.mainDiv}>
-          {womenarr.map((el, i) => {
+          {womenData.map((el, i) => {
             return (
               <div key={i}  onClick={()=>handleClick(el)} >
                 <Team {...el} />
@@ -42,7 +55,7 @@ export const Teams = ({theme}) => {
       <div className={styles.container}>
         <span>IPL 2022 TEAMS</span>
         <div className={styles.mainDiv}>
-          {iplarr.map((el, i) => {
+          {iplData.map((el, i) => {
             return (
               <div key={i}  onClick={()=>handleClick(el)}>
                 <Team {...el} />
--- Front-end/app/src/components/Teams/News.jsx
+++ Front-end/app/src/components/Teams/News.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export const News = ({context,hline,intro,imageId}) => {
-    console.log(context)
   return (
     <>
       <div>
