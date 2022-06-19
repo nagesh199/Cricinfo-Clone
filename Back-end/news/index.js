09b185bfd87911e2f8f7256e72358ec323c94d2a
rushig777 <gaikwadrushi777@gmail.com>
2022-06-19 11:32:13 +0530

    ..

--- Back-end/news/index.js
+++ Back-end/news/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, 'Front-end/app/build')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get("/teams",(req,res)=>{
  
